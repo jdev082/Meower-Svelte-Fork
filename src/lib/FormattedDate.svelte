6c488d31e12d281a86da2614136c35303bea5a9f
jdev082 <willsjayden2@gmail.com>
2023-03-12 15:47:09 -0500

    b

--- src/lib/FormattedDate.svelte
+++ src/lib/FormattedDate.svelte
@@ -33,6 +33,11 @@ ISO: ${_date.toISOString()}`;
 	.date {
 		font-style: italic;
 		cursor: help;
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		margin: 4pt;
+		padding: 2pt;
 		background: var(--orange);
 		color: var(--foreground);
 	}
